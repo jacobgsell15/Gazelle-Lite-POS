41e53db167a6b915af4d21b7b9730efce7aa6add
jacob <jacobgsell15@gmail.com>
2025-06-09 11:00:13 -0400

    up

--- frontend/src/pages/Order.js
+++ frontend/src/pages/Order.js
@@ -385,12 +385,12 @@ const DeleteButton = {
                     </select>
                 </label>
                 </div>
-                {!(props.workorder === null) && (
+                {!(props.workorder.id === null) && (
                     <>
                     <input type="submit" style={SubmitButton} onMouseEnter={() => setUpdateHover(true)} onMouseLeave={() => setUpdateHover(false)} /><button style={DeleteButton} onClick={(event) => handleDelete(event,props.workorder)} onMouseEnter={() => setDeleteHover(true)} onMouseLeave={() => setDeleteHover(false)}>Delete</button>
                     </>
                 )}
-                {(props.workorder === null) && (
+                {(props.workorder.id === null) && (
                     <>
                     <input type="submit" style={SubmitButton} onMouseEnter={() => setUpdateHover(true)} onMouseLeave={() => setUpdateHover(false)} value="Start Order" />                    
                     </>
