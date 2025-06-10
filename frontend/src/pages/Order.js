3edf2d27a12e4530b143954bae08c21e997af100
jacob <jacobgsell15@gmail.com>
2025-06-10 14:38:43 -0400

    Up

--- frontend/src/pages/Order.js
+++ frontend/src/pages/Order.js
@@ -227,7 +227,7 @@ function EditOrder(props){
         }        
         else{
             const uorder = {"id":"","location":props.location.id,"table":updatedOrder.table,"guests":updatedOrder.guests,"total":0.00,"finalized_list":{"default":true},"completed":false}           
-            const new_guests = props.location.guests + updatedOrder.guests;
+            const new_guests = props.location.guests + parseInt(updatedOrder.guests);
             const new_avail = props.location.avail - 1;
             const uloc = {"id":props.location.id,"name":props.location.name,"tables":props.location.tables,"avail":new_avail,"guests":new_guests,"staff":0,"waiting":0}
             console.log(locationHandler);
