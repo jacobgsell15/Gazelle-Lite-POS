0bdaf67e2cb186d1729c70cf60dfb0ed80773119
jacob <jacobgsell15@gmail.com>
2025-06-18 00:26:07 -0400

    Up

--- frontend/src/pages/Order.js
+++ frontend/src/pages/Order.js
@@ -1,101 +1,8 @@
 import {useState} from 'react';
 import {useEffect} from 'react';
-import {StaxJs, staxJs} from 'https://staxjs.staxpayments.com/stax.js';
 import axios from 'axios'
 
 function PaymentField(props){
-new StaxJs('Covestone-Corporations-67ca113d35e80f9e7c', {
-  number: {
-    id: 'card-number',
-    placeholder: '0000 0000 0000 0000',
-    style: 'width: 90%; height:90%; border-radius: 3px; border: 1px solid #ccc; padding: .5em .5em; font-size: 91%;'
-  },
-  cvv: {
-    id: 'card-cvv',
-    placeholder: '000',
-    style: 'width: 30px; height:90%; border-radius: 3px; border: 1px solid #ccc; padding: .5em .5em; font-size: 91%;'
-  }
-});
-
-staxJs.showCardForm().then(handler => {
-  console.log('form loaded');
-  handler.setTestPan('4111111111111111');
-  handler.setTestCvv('123');
-  var form = document.querySelector('form');
-  form.querySelector('input[name=month]').value = 11;
-  form.querySelector('input[name=year]').value = 2025;
-  form.querySelector('input[name=cardholder-first-name]').value = 'Jon';
-  form.querySelector('input[name=cardholder-last-name]').value = 'Doe';
-  form.querySelector('input[name=phone]').value = '+1111111111111111';
-})
-.catch(err => {
-  console.log('error init form ' + err);
-  // reinit form
-});
-
-document.querySelector('#paybutton').onclick = () => {
-
-  var form = document.querySelector('form');
-  var extraDetails = {
-    total: 1, // 1$
-    firstname: form.querySelector('input[name=cardholder-first-name]').value,
-    lastname: form.querySelector('input[name=cardholder-last-name]').value,
-    company: '',
-    email: '',
-    month: form.querySelector('input[name=month]').value,
-    year: form.querySelector('input[name=year]').value,
-    phone: form.querySelector('input[name=phone]').value,
-    address_1: '100 S Orange Ave',
-    address_2: '',
-    address_city: 'Orlando',
-    address_state: 'FL',
-    address_zip: '32811',
-    address_country: 'USA',
-    url: "https://app.staxpayments.com/#/bill/",
-    method: 'card',
-    // validate is optional and can be true or false. 
-    // determines whether or not stax.js does client-side validation.
-    // the validation follows the sames rules as the api.
-    // check the api documentation for more info:
-    // https://staxpayments.com/api-documentation/
-    validate: false,
-    // meta is optional and each field within the POJO is optional also
-    meta: {
-      reference: 'invoice-reference-num',// optional - will show up in emailed receipts
-      memo: 'notes about this transaction',// optional - will show up in emailed receipts
-      otherField1: 'other-value-1', // optional - we don't care
-      otherField2: 'other-value-2', // optional - we don't care
-      subtotal: 1, // optional - will show up in emailed receipts
-      tax: 0, // optional - will show up in emailed receipts
-      lineItems: [ // optional - will show up in emailed receipts
-        {"id": "optional-fm-catalog-item-id", "item":"Demo Item","details":"this is a regular, demo item","quantity":10,"price":.1}
-      ] 
-    }
-  };
-  
-  console.log(extraDetails);
-
-  // call pay api
-  staxJs.pay(extraDetails).then((result) => {
-    console.log('pay:');
-    console.log(result);
-    if (result.id) {
-      console.log('success')
-    }
-  })
-  .catch(err => {
-    // if a transaction did occur, but errored, the error will be in the message of the first child transactoin
-    if (err.payment_attempt_message) {
-      console.log('error')
-    } else {
-      // else, there may have been a validation error - and tokenization failed
-      // err can contain an object where each key is a field name that points to an array of errors
-      // such as {phone_number: ['The phone number is invalid']}
-      console.log('error')
-    }
-  });
-}
-
 return (
 <>
   <form onsubmit="return false;">
@@ -117,10 +24,7 @@ return (
     <div class="group">
       <label>
         <span>Card</span>
-        <div id="card-element" class="field">
-          <div id="staxjs-number" style="width:180px; height:35px; display: inline-block; margin:3px"></div>
-          <div id="staxjs-cvv" style="width:50px; height:35px; display: inline-block; margin:3px"></div>
-        </div>
+        <input name="phone" class="field input-box"  placeholder="+1000000000000" />
         <div style="width:40px; height:35px; display: inline-block;">
           <input name="month" size="3" maxlength="2" placeholder="MM" style="width: 22px; height:18px; border-radius: 3px; border: 1px solid #ccc; padding: .5em .5em; font-size: 91%;" />
         </div>
