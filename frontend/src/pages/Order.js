4c3397b6ef61659caa267e0c43d584abd67bfa37
jacob <jacobgsell15@gmail.com>
2025-06-18 00:00:26 -0400

    ?

--- frontend/src/pages/Order.js
+++ frontend/src/pages/Order.js
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import {useEffect} from 'react';
+import {StaxJs} from 'https://staxjs.staxpayments.com/stax.js';
 import axios from 'axios'
 
 function PaymentField(props){
