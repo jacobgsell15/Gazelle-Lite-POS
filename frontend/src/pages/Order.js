1f01e97e17048fd49db786295b3b53d7767cd668
jacob <jacobgsell15@gmail.com>
2025-06-18 09:12:27 -0400

    Up

--- frontend/src/pages/Order.js
+++ frontend/src/pages/Order.js
@@ -61,12 +61,12 @@ const PayOrderSubHeadingDiv = {
     padding:"15px 0px 0px 15px" 
 }
 const DivL = {
-    width:"60%",
+    width:"50%",
     height:"100%",
     float:"left"
 }
 const DivR = {
-    width:"30%",
+    width:"50%",
     height:"100%",
     float:"left"
 }
